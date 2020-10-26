f2912be603aa0270215b5a8aa5f36992af3e9f34
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 21:35:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 //return message 
 
 function takeANumber(deliLine, person){
-  deliLine.push(person)
-  return `Welcome, ${person}. You are number ${deliLine.length} in line.`
+  deliLine.push(person);
+  return `Welcome, ${person}. You are number ${deliLine.length} in line.`;
 }
 
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length === 0){
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }else{
     var currentCustomer = katzDeliLine.shift();
-    return `Currently serving ${currentCustomer}.`
+    return `Currently serving ${currentCustomer}.`;
   }
 }
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
-    return "The line is currently empty."
+    return "The line is currently empty.";
   } else{
-    var string = "The line is currently: "
+    var string = "The line is currently: ";
     for(let i=1; i<katzDeliLine.length; i++){
-      return `${string}${i}. ${katzDeliLine[i-1]}, ${i+1}. ${katzDeliLine[i]}, ${i+2}. ${katzDeliLine[i+1]}`
+      return `${string}${i}. ${katzDeliLine[i-1]}, ${i+1}. ${katzDeliLine[i]}, ${i+2}. ${katzDeliLine[i+1]}`;
     }
   }
 }
