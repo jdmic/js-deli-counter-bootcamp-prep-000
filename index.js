a840f730a839682b93967b8483e37114031d5edb
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 21:30:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+
+//apend new person to back of line
+//return message 
+
+function takeANumber(deliLine, person){
+  deliLine.push(person)
+  return `Welcome, ${person}. You are number ${deliLine.length} in line.`
+}
+
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }else{
+    var currentCustomer = katzDeliLine.shift();
+    return `Currently serving ${currentCustomer}.`
+  }
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } else{
+    var string = "The line is currently: "
+    for(let i=0; i<katzDeliLine.length; i++){
+      return `${string} ${i+1}. ${katzDeliLine[i]} ${i+2}. ${katzDeliLine[i+1]}, ${i+3}. ${katzDeliLine[i+2]`
+    }
+  }
+}
